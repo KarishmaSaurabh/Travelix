0827e30a14eedf503f84f38b53a103146744082d
Karishma Saurabh <karishmasaurabh998@gmail.com>
2021-06-20 18:14:44 +0530

    Initial import

---
+++ script.js
@@ -0,0 +1,47 @@
+const counters = document.querySelectorAll('.counter');
+const speed = 400;
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        const inc = target / speed;
+
+        if (count < target) {
+
+            counter.innerText = count + inc;
+
+            setTimeout(updateCount, 5);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCount();
+});
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
+menu_item.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
